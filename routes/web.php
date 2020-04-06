8be593dbb5f8c4bfd2feafe8cc9a32ae5dba75b3
Frank Pillukeit <clients@pw-websolutions.com>
2020-04-06 22:57:48 +0200

    Updates routes for address types

--- routes/web.php
+++ routes/web.php
@@ -6,28 +6,19 @@ Route::group(['prefix' => 'localisation', 'namespace' => 'PWWEB\Localisation\Con
     Route::get('/address/store', 'AddressController@store')->name('localisation.address.store');
 });
 
-Route::group(
-    [
-        'prefix' => 'addresses',
-        'namespace' => '\PWWEB\Localisation\Controllers'
-    ],
-    function () {
-        Route::group(
-            [
-                'prefix' => 'types',
-                'namespace' => '\PWWEB\Localisation\Controllers\Address'
-            ],
-            function () {
-                Route::get('/', 'TypeController@index')->name('localisation.address.type.index');
-                Route::get('/show/{type}', 'TypeController@show')->name('localisation.address.type.show');
-                Route::get('/edit/{type}', 'TypeController@edit')->name('localisation.address.type.edit');
-                Route::get('/create', 'TypeController@create')->name('localisation.address.type.create');
-                Route::patch('/update/{type}', 'TypeController@update')->name('localisation.address.type.update');
-                Route::delete('/destroy/{type}', 'TypeController@destroy')->name('localisation.address.type.destroy');
-                Route::post('/store', 'TypeController@store')->name('localisation.address.type.store');
-            }
-        );
-
-        Route::get('/', 'AddressController@index');
-    }
-);
+Route::namespace('PWWEB\Localisation\Controllers')
+    ->name('localisation.')
+    // ->middleware('auth')
+    ->group(
+        function () {
+            Route::namespace('Address')
+                ->prefix('address')
+                ->name('address.')
+                // ->middleware('auth')
+                ->group(
+                    function () {
+                        Route::resource('type', TypeController::class);
+                    }
+                );
+        }
+    );
